540e1e989f9443bca2047bb96c43cdc9cac82d72
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-06-20 22:58:25 +0900

    update qr code + sameple Setting

---
+++ AppClient/App.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Platform, StyleSheet, View, Text} from 'react-native';
+
+import { QRCode } from 'react-native-custom-qr-codes-expo';
+
+/*
+const instructions = Platform.select({
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+});*/
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Save Scanner</Text>
+      <Text style={styles.instructions}>Share QR Code for us!</Text>
+      <QRCode content='https://reactnative.com'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
