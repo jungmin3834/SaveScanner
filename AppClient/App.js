6bfd22e24d4a691104ddfe23c935d66189321f4d
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-06-22 15:17:38 +0900

    react native 정리 및 stack /navigation add

--- AppClient/App.js
+++ AppClient/App.js
@@ -1,40 +1,24 @@
 import * as React from 'react';
-import { Platform, StyleSheet, View, Text} from 'react-native';
+import { Platform, StyleSheet, View, Text,AsyncStorage} from 'react-native';
+import Home from './screen/home';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { QRCode } from 'react-native-custom-qr-codes-expo';
+const Stack = createStackNavigator();
 
-/*
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});*/
-
-export default function App() {
+function MyStack() {
   return (
-    <View style={styles.container}>
-            <Text style={styles.welcome}>Save Scanner</Text>
-      <Text style={styles.welcome}>Save Scanner</Text>
-      <Text style={styles.instructions}>Share QR Code for us!</Text>
-      <QRCode content='sadsadsatnative.com'/>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class App extends React.Component {
+  render(){
+    return (
+      <MyStack> </MyStack>
+    );
+  }
+}
+
+
