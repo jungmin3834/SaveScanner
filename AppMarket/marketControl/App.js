c89b75e378f45fcf8ae62c2fab3e81d11e95b8b9
DESKTOP-V0G0BE3\sbj38 <sbj3834@naver.com>
2020-07-26 12:04:03 +0900

    Update Modal System for list Event // 디자인만 1차적으로 완성

--- AppMarket/marketControl/App.js
+++ AppMarket/marketControl/App.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import MainScreen from './src/pages/MainScreen';
+import Modal from './src/modals/modal';
 
 const App: () => React$Node = () => {
   return (
     <>
-       <MainScreen></MainScreen>
+       <Modal></Modal>
     </>
   );
 };
---
+++ AppMarket/marketControl/src/modals/modal.js
@@ -0,0 +1,109 @@
+import React, {useState} from 'react';
+import {Button, Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import Modal from 'react-native-modal';
+
+function ModalTester() {
+  const [isModalVisible, setModalVisible] = useState(false);
+  
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+    return (
+      <View style={{flex: 1}}>
+        <Button title="Show modal" onPress={toggleModal} />
+
+        <Modal isVisible={isModalVisible}>
+          <View style={styles.container}>
+              <View style={styles.body}>
+                <View style={styles.items}>
+                    <View style={styles.row}>
+                        <Text style={styles.itemTitle}>
+                            출입 시간
+                        </Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={styles.itemTitle}>
+                                NAME
+                        </Text>
+                    </View>
+                    <View style={styles.row}>
+                        <TouchableOpacity style={styles.item} title="Hide modal" onPress={toggleModal} >
+                            <Text style={styles.itemTitle}>
+                                전화걸기
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.row}>
+                        <TouchableOpacity style={styles.item} title="Hide modal" onPress={toggleModal} >
+                            <Text style={styles.itemTitle}>
+                                이메일 작성
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.modalButton}>
+                    <TouchableOpacity style={styles.closeModal} title="Hide modal" onPress={toggleModal} >
+                        <Text style={styles.closeBtn}>
+                            Close
+                        </Text>
+                     </TouchableOpacity>
+                </View>
+              </View>
+          </View>
+        </Modal>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    body :{  
+        height:"100%",
+        width:"100%",
+    },
+    items: {
+      height:"24%",
+      width:"100%",
+      marginTop:"45%",
+      backgroundColor: '#efefef',
+      margin:"auto",
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    itemTitle:{
+        fontSize : 20,
+        textAlign:"center",
+        backgroundColor:"#efefef"
+    },
+    row:{
+        width:"80%",
+        backgroundColor:"grey",
+        marginTop:10,
+    },
+    modalButton:{
+      width:"100%",
+      height:"10%",
+      backgroundColor:"grey",
+      margin:"auto", 
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    closeBtn:{
+        fontSize:25
+    },
+    closeModal:{
+        borderRadius: 100/2,
+        width:"70%",
+        height:"50%",
+        backgroundColor:"#efefef",
+        margin:"auto", 
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+    
+  });
+
+export default ModalTester;
